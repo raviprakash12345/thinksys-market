33d2029c98bf754d5f2c9ebe8ca9dc8a5b2ba416
ManoharRao13 <rao.manohar@thinksys.com>
2023-01-18 15:08:59 +0530

    table page done

--- src/client/modules/dataTable/view.js
+++ src/client/modules/dataTable/view.js
@@ -1,8 +1,33 @@
-const DataTableView = ()=>{
+import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
 
-    return (
-    <></>
-    )
+import TableData from "../shared/tableData";
+
+
+
+
+let defaultColor = "#e35981";
+let defaultLightColor = "#f6dbe9";
+
+const DataTableView = ( {
+    isDrawerMargin = true,
+    themeColor = "#e35981",
+    lightColor = "",
+    isTable = false,})=>{
+        
+      return (
+        <Box sx={!isDrawerMargin && { marginLeft: "250px" }}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Typography sx={{ color: "#b22362" }} variant="h4">
+              Data Table
+            </Typography>
+            <Typography sx={{ marginLeft: "16px" }} variant="body1">
+              App / Tables / Data-Table
+            </Typography>
+          </Box>
+         <TableData/>
+        </Box>
+      );
 }
 
 
---
+++ src/client/modules/shared/tableData/index.js
@@ -0,0 +1,116 @@
+import TableDataView from "./view";
+import MessageIcon from "@mui/icons-material/Message";
+import Box from "@mui/system/Box";
+import { Button, Divider } from "@mui/material";
+import LinearProgress from "@mui/material/LinearProgress";
+const dummyData = [
+  {
+    name: "Gabby George",
+    title : "Business Analyst",
+    subName: "task description 1",
+    kpi: <LinearProgress variant="determinate" value={30} color="primary" />,
+    button : <Button  sx={{backgroundColor :"#3d69fa",color :"white",borderRadius:"20px"}}>Active</Button>,
+    salary : "$10,000.00",
+  },
+  {
+    name: "Aiden Lloyd",
+    title : "Business Consultant",
+    subName: "task description 1",
+    kpi: <LinearProgress variant="determinate" value={10} color="primary" />,
+    button : <Button  sx={{backgroundColor :"#3d69fa",color :"white",borderRadius:"20px"}}>Active</Button>,
+    salary : "$20,000.00",
+  },
+  {
+    name: "Jaden Collins",
+    title : "Attorney",
+    subName: "task description 1",
+    kpi: <LinearProgress variant="determinate" value={15} color="primary" />,
+    button : <Button  sx={{backgroundColor :"#3d69fa",color :"white",borderRadius:"20px"}}>Active</Button>,
+    salary : "$20,000.00",
+  },
+  {
+    name: "Franky Rees",
+    title : "Business Consultant",
+    subName: "task description 1",
+    kpi: <LinearProgress variant="determinate" value={30} color="primary" />,
+    button : <Button  sx={{backgroundColor :"#3d69fa",color :"white",borderRadius:"20px"}}>Non Active</Button>,
+    salary : "$10,000.00",
+  },
+  {
+    name: "Aaren Rose",
+    title : "Business Consultant",
+    subName: "task description 1",
+    kpi: <LinearProgress variant="determinate" value={2} color="primary" />,
+    button : <Button  sx={{backgroundColor :"#3d69fa",color :"white",borderRadius:"20px"}}>Active</Button>,
+    salary : "$40,000.00",
+  },
+  {
+    name: "Aiden Lloyd",
+    title : "Business Consultant",
+    subName: "task description 1",
+    kpi: <LinearProgress variant="determinate" value={10} color="primary" />,
+    button : <Button  sx={{backgroundColor :"#3d69fa",color :"white",borderRadius:"20px"}}>Non Active</Button>,
+    salary : "$40,000.00",
+  },
+  {
+    name: "Aiden Lloyd",
+    title : "Business Consultant",
+    subName: "task description 1",
+    kpi: <LinearProgress variant="determinate" value={15} color="primary" />,
+    button : <Button  sx={{backgroundColor :"#3d69fa",color :"white",borderRadius:"20px"}}>Active</Button>,
+    salary : "$70,000.00",
+  },
+
+  
+];
+const TableData = ({ themeColor = "e35981" }) => {
+  const columnConfigs = [
+    {
+      id: "name",
+      label: "Name",
+      field: "name",
+      canSort: false,
+      noResize: false,
+    },
+    {
+      id: "title",
+      label: "Title",
+      field: "title",
+      canSort: false,
+      noResize: false,
+    },
+    {
+      id: "kpi",
+      label: "KPI",
+      field: "kpi",
+      canSort: false,
+      noResize: false,
+    },
+    {
+      id: "button",
+      label: "Status",
+      field: "button",
+      canSort: false,
+      noResize: false,
+    },
+    {
+      id: "salary",
+      label: "Salary",
+      field: "salary",
+      canSort: false,
+      noResize: false,
+    },
+ 
+  ];
+  return (
+    <>
+      <TableDataView
+        columnConfigs={columnConfigs}
+        entries={dummyData}
+        themeColor={themeColor}
+      />
+    </>
+  );
+};
+
+export default TableData;
